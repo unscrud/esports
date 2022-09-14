ad9aa8873d859285af290c26e94767349ed5ba61
Daniel Carvalho <daniel.devop@gmail.com>
2022-09-14 19:20:40 -0300

    Limpeza

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Olá Mundo!</Text>
-      <StatusBar style="auto" />
+    <View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
