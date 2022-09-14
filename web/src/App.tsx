ed1deae5f0deea21ac613215b249d571fe82dcce
Daniel Carvalho <daniel.devop@gmail.com>
2022-09-14 18:13:59 -0300

    Adição de faixa inferior com botão à direita

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -52,6 +52,18 @@ function App() {
           </div>
         </a>
       </div>
+
+      <div className='pt-1 bg-duo-gradient self-stretch rounded-lg mt-8 overflow-hidden'>
+        <div className='bg-[#2A2634] px-8 py-6 flex justify-between items-center'>
+          <div>
+            <strong className='text-2xl text-white font-black block'>Não encontrou seu duo?</strong>
+            <span className='text-zinc-400 block'>Publique um anúncio para encontrar novos playes!</span>
+          </div>
+
+          <button className='py-3 px-4 bg-violet-500 hover:bg-violet-700 text-white rounded'>
+            Publicar anúncio</button>
+        </div>
+      </div>
     </div>
   )
 }
