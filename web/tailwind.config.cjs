f11eece18e1b0953bdc71d872021fb9dae3871bf
Daniel Carvalho <daniel.devop@gmail.com>
2022-09-14 17:49:39 -0300

    Gradiente para as figuras

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -6,7 +6,9 @@ module.exports = {
       backgroundImage: {
         galaxy: "url('/background-galaxy.png')",
         'duo-gradient':
-          'linear-gradient(89.86deg, #9572FC 23.08%, #43E7AD 33.94%, #E1D55D 44.57%)'
+          'linear-gradient(89.86deg, #9572FC 23.08%, #43E7AD 33.94%, #E1D55D 44.57%)',
+        'game-gradient':
+          'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)'
       }
     }
   },
