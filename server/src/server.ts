9b7dfd06229b006addf7e452b6d619fab3996052
Daniel Carvalho <daniel.devop@gmail.com>
2022-09-15 15:18:10 -0300

    Criacao das principais rotas

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,12 +2,32 @@ import express from 'express'
 
 const app = express()
 
-app.get('/ads', (request, response) => {
+app.get('/games', (request, response) => {
+  return response.json([
+    { id: 1, name: 'Game 1' },
+    { id: 2, name: 'Game 2' },
+    { id: 3, name: 'Game 3' }
+  ]);
+});
+
+app.post('/ads', (request, response) => {
+  return response.status(201).json([]);
+});
+
+app.get('/games/:id/ads', (request, response) => {
   return response.json([
     { id: 1, name: 'Anúncio 1' },
     { id: 2, name: 'Anúncio 2' },
-    { id: 3, name: 'Anúncio 3' }
-  ])
-})
+    { id: 3, name: 'Anúncio 3' },
+    { id: 4, name: 'Anúncio 4' },
+    { id: 5, name: 'Anúncio 5' },
+    { id: 6, name: 'Anúncio 6' }
+  ]);
+});
+
+
+app.get('/ads/:id/discord', (request, response) => {
+  return response.json([]);
+});
 
 app.listen(3333)
