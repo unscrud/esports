1023eb41c893b2036cfab6e892642fe9a67fb379
Daniel Carvalho <daniel.devop@gmail.com>
2022-09-15 12:01:56 -0300

    adiciona espaço no inicio e no fim da lista

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -26,6 +26,7 @@ export function Home() {
         )}
         showsHorizontalScrollIndicator={false}
         horizontal
+        contentContainerStyle={styles.contentList}
       />
     </View>
   );
