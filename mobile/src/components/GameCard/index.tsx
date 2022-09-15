bb88b02f5534373c520d56935ff3b7e7a83085df
Daniel Carvalho <daniel.devop@gmail.com>
2022-09-15 09:56:44 -0300

    passando outras props do touchable opacity

--- mobile/src/components/GameCard/index.tsx
+++ mobile/src/components/GameCard/index.tsx
@@ -1,4 +1,8 @@
-import { TouchableOpacity, ImageBackground, ImageSourcePropType } from 'react-native';
+import { 
+  TouchableOpacity,
+  TouchableOpacityProps,
+  ImageBackground,
+  ImageSourcePropType } from 'react-native';
 import { styles } from './styles';
 
 export interface GameCardProps {
@@ -8,14 +12,17 @@ export interface GameCardProps {
   cover: ImageSourcePropType;
 }
 
-interface Props {
+interface Props extends TouchableOpacityProps {
   data: GameCardProps;
 }
 
-export function GameCard({data}: Props) {
+export function GameCard({data, ...rest}: Props) {
   return (
-    <TouchableOpacity style={styles.container}>
-      
+    <TouchableOpacity style={styles.container} {...rest}>
+      <ImageBackground 
+        style={styles.cover}
+        source={data.cover}
+      />
     </TouchableOpacity>
   );
 }
