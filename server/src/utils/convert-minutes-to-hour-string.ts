fab32e5e85c9091bed689db2b70e81cf269e37b0
Daniel Carvalho <daniel.devop@gmail.com>
2022-09-15 19:44:51 -0300

    Formatando hora para não ficar um numero por bloco

--- server/src/utils/convert-minutes-to-hour-string.ts
+++ server/src/utils/convert-minutes-to-hour-string.ts
@@ -2,5 +2,5 @@ export function convertMinutesToHourString(minutesAmount: number){
   const hours  = Math.floor(minutesAmount / 60);
   const minutes = minutesAmount % 60;
 
-  return `${hours}:${minutes}`;
+  return `${String(hours).padStart(2,"0")}:${String(minutes).padEnd(2,"0")}`;
 }
