64e4300d07fac9a7b552ffe40da22b50cd264972
Daniel Carvalho <daniel.devop@gmail.com>
2022-09-15 09:50:13 -0300

    interface com props da imagem do gamecard

--- mobile/src/components/GameCard/index.tsx
+++ mobile/src/components/GameCard/index.tsx
@@ -1,10 +1,17 @@
-import { TouchableOpacity } from 'react-native';
+import { TouchableOpacity, ImageBackground, ImageSourcePropType } from 'react-native';
 import { styles } from './styles';
+
+export interface GameCardProps {
+  id: string;
+  name: string;
+  ads: string;
+  cover: ImageSourcePropType;
+}
 
 export function GameCard() {
   return (
     <TouchableOpacity style={styles.container}>
-
+      
     </TouchableOpacity>
   );
 }
