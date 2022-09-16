c79cbde81dfcebb4ef03ac3ca16cf8e0f388c452
Daniel Carvalho <daniel.devop@gmail.com>
2022-09-16 15:40:22 -0300

    define um efeito colateral da alt do estado sem renderização da tela

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -11,6 +11,11 @@ function App() {
     setHasUserClickedButton(true);
   }
 
+  useEffect(
+    () => {console.log("teste")},
+    [hasUserClickedButton]
+  );
+
   return (
     <div className='max-w-[1344px] mx-auto flex flex-col items-center my-20'>
       <img src={logoImg} alt="" />
