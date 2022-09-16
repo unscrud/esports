8b6a4e6c1834945394bacf2632f6fe70f7f349b8
Daniel Carvalho <daniel.devop@gmail.com>
2022-09-16 18:07:31 -0300

    formatação do modal

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -44,11 +44,11 @@ function App() {
       <Dialog.Root>
         <CreateAdBanner />
         <Dialog.Portal>
-              <Dialog.Overlay />
-              <Dialog.Content>
-                <Dialog.Title>Publique um anúncio</Dialog.Title>
+              <Dialog.Overlay className='bg-black/60 inset-0 fixed' />
+              <Dialog.Content className='fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-lg shadow-black/50'>
+                <Dialog.Title className='text-3xl text-white font-black'>Publique um anúncio</Dialog.Title>
                 <Dialog.Content>
-                  
+
                 </Dialog.Content>
               </Dialog.Content>
             </Dialog.Portal>
