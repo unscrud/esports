3d5bb2421198feac5882afc6c3c163e211116168
Daniel Carvalho <daniel.devop@gmail.com>
2022-09-16 18:34:16 -0300

    content duplicado

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -48,7 +48,6 @@ function App() {
               <Dialog.Overlay className='bg-black/60 inset-0 fixed' />
               <Dialog.Content className='fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-lg shadow-black/50'>
                 <Dialog.Title className='text-3xl text-white font-black'>Publique um anúncio</Dialog.Title>
-                <Dialog.Content>
                   <form>
                     <div>
                       <label htmlFor="game">Qual é o game?</label>
@@ -99,7 +98,6 @@ function App() {
                       </button>
                     </footer>
                   </form>
-                </Dialog.Content>
               </Dialog.Content>
             </Dialog.Portal>
       </Dialog.Root>
