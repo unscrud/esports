9c8026d25510c60696339bc682ac840390e94869
Daniel Carvalho <daniel.devop@gmail.com>
2022-09-16 15:59:46 -0300

    Removendo banners estáticos

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -29,12 +29,7 @@ function App() {
       <h1 className='text-6xl text-white font-black mt-20'>Seu <span className='text-transparent bg-duo-gradient bg-clip-text'>duo</span> está aqui.</h1>
 
       <div className='grid grid-cols-6 gap-6 mt-16'>
-        <GameBanner bannerUrl="/game-1.png" title="title 1" adsCount={1} />
-        <GameBanner bannerUrl="/game-2.png" title="title 2" adsCount={2} />
-        <GameBanner bannerUrl="/game-3.png" title="title 3" adsCount={3} />
-        <GameBanner bannerUrl="/game-4.png" title="title 4" adsCount={4} />
-        <GameBanner bannerUrl="/game-5.png" title="title 5" adsCount={5} />
-        <GameBanner bannerUrl="/game-6.png" title="title 6" adsCount={6} />
+        
       </div>
 
       <CreateAdBanner />
