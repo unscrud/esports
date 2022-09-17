02bd5a47d627b7ee36f2717ca2a89cb45e80490e
Daniel Carvalho <daniel.devop@gmail.com>
2022-09-17 15:43:12 -0300

    usando interface da rota game

--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -2,10 +2,11 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import { Background } from '../../components/Background';
 import { styles } from './styles';
 import { useRoute } from "@react-navigation/native";
+import { GameParams } from '../../@types/navigation';
 
 export function Game() {
   const route = useRoute()
-  const game = route.params
+  const game = route.params as GameParams
   console.log(game)
   return (
     <Background>
