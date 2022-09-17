967555b6e4f5a67efc91fc464e8cfe047fd7bb62
Daniel Carvalho <daniel.devop@gmail.com>
2022-09-17 16:14:25 -0300

    novos imports

--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -3,11 +3,15 @@ import { Background } from '../../components/Background';
 import { styles } from './styles';
 import { useRoute } from "@react-navigation/native";
 import { GameParams } from '../../@types/navigation';
+import { Image, TouchableOpacity, View } from "react-native";
+import { Entypo } from "@expo/vector-icons";
+import { THEME } from '../../theme';
+import { logoImg } from "../../assets/logo-nlw-esports.png";
 
 export function Game() {
   const route = useRoute()
   const game = route.params as GameParams
-  console.log(game)
+
   return (
     <Background>
       <SafeAreaView style={styles.container}>
