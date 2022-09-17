69bd3c6d0534fb51ada14186db0a06b861d5a89e
Daniel Carvalho <daniel.devop@gmail.com>
2022-09-17 15:38:02 -0300

    recebendo dados da tela anterior quando um game é tocado

--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -1,13 +1,17 @@
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Background } from '../../components/Background';
 import { styles } from './styles';
+import { useRoute } from "@react-navigation/native";
 
 export function Game() {
+  const route = useRoute()
+  const game = route.params
+  console.log(game)
   return (
     <Background>
-    <SafeAreaView style={styles.container}>
+      <SafeAreaView style={styles.container}>
 
-    </SafeAreaView>
+      </SafeAreaView>
     </Background>
   );
 }
