366a11aa612ff0b0f3c8daff26e1a3cb69fdb4f3
Daniel Carvalho <daniel.devop@gmail.com>
2022-09-17 14:58:47 -0300

    chamando home pela rota

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -7,7 +7,7 @@ import {
   Inter_700Bold,
   Inter_900Black
 } from '@expo-google-fonts/inter';
-import { Home } from './src/screens/Home';
+import { Routes } from './src/routes';
 import { Loading } from './src/components/Loading';
 
 export default function App() {
@@ -26,7 +26,7 @@ export default function App() {
         translucent
       />
 
-      {fontsLoaded ? <Home /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </Background>
   );
 }
