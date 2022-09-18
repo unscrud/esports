d56d5093176ae67696ea79984556963c881e55a6
Daniel Carvalho <daniel.devop@gmail.com>
2022-09-18 10:56:48 -0300

    adicionando colorValue opcional à props

--- mobile/src/components/DuoInfo/index.tsx
+++ mobile/src/components/DuoInfo/index.tsx
@@ -1,13 +1,14 @@
-import { View, Text } from 'react-native';
+import { View, Text, ColorValue } from 'react-native';
 
 import { styles } from './styles';
 
 interface Props {
   label: string;
   value: string;
+  colorValue?: ColorValue;
 }
 
-export function DuoInfo({label, value}: Props) {
+export function DuoInfo({label, value, colorValue}: Props) {
   return (
     <View style={styles.container}>
       <Text style={styles.label}>{label}</Text>
