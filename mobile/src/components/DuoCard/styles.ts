e780c4a2bf66f71f716a9654bbf72ed69551ee02
Daniel Carvalho <daniel.devop@gmail.com>
2022-09-18 13:36:28 -0300

    Estilização do botão do game card

--- mobile/src/components/DuoCard/styles.ts
+++ mobile/src/components/DuoCard/styles.ts
@@ -8,5 +8,20 @@ export const styles = StyleSheet.create({
     borderRadius: 8,
     padding: 20,
     marginRight: 16
+  },
+  button: {
+    width: 140,
+    height: 36,
+    borderRadius: 6,
+    backgroundColor: THEME.COLORS.PRIMARY,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonTitle: {
+    color: THEME.COLORS.TEXT,
+    fontFamily: THEME.FONT_FAMILY.SEMI_BOLD,
+    fontSize: THEME.FONT_SIZE.SM,
+    marginLeft: 8
   }
 });
