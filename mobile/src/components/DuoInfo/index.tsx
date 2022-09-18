08f32d54fb94bb0f1644e2fba0dd5990a1f2fe9f
Daniel Carvalho <daniel.devop@gmail.com>
2022-09-18 12:27:25 -0300

    impedindo quebra de linha

--- mobile/src/components/DuoInfo/index.tsx
+++ mobile/src/components/DuoInfo/index.tsx
@@ -14,7 +14,13 @@ export function DuoInfo({label, value,
   return (
     <View style={styles.container}>
       <Text style={styles.label}>{label}</Text>
-      <Text style={[styles.value, {color: colorValue}]}>{value}</Text>
+      
+      <Text 
+        style={[styles.value, {color: colorValue}]}
+        numberOfLines={1}
+      >
+        {value}
+      </Text>
     </View>
   );
 }
