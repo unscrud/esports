b8fd4e1af329c8bde68a8e066eb7eec27bb49440
Daniel Carvalho <daniel.devop@gmail.com>
2022-09-18 11:29:05 -0300

    interface para receber dados do backend

--- mobile/src/components/DuoCard/index.tsx
+++ mobile/src/components/DuoCard/index.tsx
@@ -2,6 +2,16 @@ import { View } from 'react-native';
 
 import { styles } from './styles';
 
+export interface DuoCardProps {
+  id: string;
+  hourEnd: string;
+  hourStart: string;
+  name: string;
+  useVoiceChannel: boolean;
+  weekDays: string[];
+  yearsPlayng: number;
+}
+
 export function DuoCard() {
   return (
     <View style={styles.container}>
