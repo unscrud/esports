71e53ab2811140c6e1245048e0332ff04016ef6b
Daniel Carvalho <daniel.devop@gmail.com>
2022-09-18 10:54:57 -0300

    criando interface props e usando nos textos

--- mobile/src/components/DuoInfo/index.tsx
+++ mobile/src/components/DuoInfo/index.tsx
@@ -2,11 +2,16 @@ import { View, Text } from 'react-native';
 
 import { styles } from './styles';
 
-export function DuoInfo() {
+interface Props {
+  label: string;
+  value: string;
+}
+
+export function DuoInfo({label, value}: Props) {
   return (
     <View style={styles.container}>
-      <Text style={styles.label}>{}</Text>
-      <Text style={styles.label}>{}</Text>
+      <Text style={styles.label}>{label}</Text>
+      <Text style={styles.value}>{value}</Text>
     </View>
   );
 }
