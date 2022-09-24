bc8e2ce476f8110f99be7aa2ca4c4cbf0dc42d0d
Daniel Carvalho <daniel.devop@gmail.com>
2022-09-24 11:34:49 -0300

    exibindo games no select

--- web/src/components/CreateAdModal.tsx
+++ web/src/components/CreateAdModal.tsx
@@ -28,6 +28,9 @@ export function CreateAdModal() {
               className='bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc-500 appearance-none'
             >
               <option disabled selected value="">Selecione o game que deseja jogar</option>
+              {games.map(game => {
+                return <option key={game.id}>{game.title}</option>
+              })}
             </select>
           </div>
 
