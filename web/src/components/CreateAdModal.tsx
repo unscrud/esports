88fcf08ac438a8de5474e09aa5649146b7406613
Daniel Carvalho <daniel.devop@gmail.com>
2022-09-24 07:14:03 -0300

    trocando o checkbox padrao por um do radix

--- web/src/components/CreateAdModal.tsx
+++ web/src/components/CreateAdModal.tsx
@@ -1,6 +1,7 @@
-import { GameController } from 'phosphor-react';
+import { Check, GameController } from 'phosphor-react';
 import { Input } from './Form/Input';
 import * as Dialog from "@radix-ui/react-dialog";
+import * as Checkbox from "@radix-ui/react-checkbox";
 
 export function CreateAdModal() {
   return (
@@ -52,8 +53,12 @@ export function CreateAdModal() {
             </div>
           </div>
 
-          <div className='mt2 flex gap-2 text-sm'>
-            <input type="checkbox" />
+          <div className='mt2 flex items-center gap-2 text-sm'>
+            <Checkbox.Root className='w-6 h-6 p-1 rounded bg-zinc-900'>
+              <Checkbox.Indicator>
+                <Check className='w-4 h-4 text-emerald-400' />
+              </Checkbox.Indicator>
+            </Checkbox.Root>
             Costumo me conectar ao chat de voz
           </div>
 
