7d5a20a4339918fca10c2451bfa5a4da4e3410ce
Daniel Carvalho <daniel.devop@gmail.com>
2022-10-01 08:30:41 -0300

    Usando o keen-slider

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -7,9 +7,19 @@ import { CreateAdModal } from "./components/CreateAdModal";
 import * as Dialog from "@radix-ui/react-dialog";
 import { Game } from "./interfaces/Game"
 import axios from 'axios';
+import 'keen-slider/keen-slider.min.css'
+import { useKeenSlider } from 'keen-slider/react'
 
 function App() {
   const [games, setGames] = useState<Game[]>([]);
+  const [ref] = useKeenSlider<HTMLDivElement>({
+    loop: true,
+    mode: "free",
+    slides: {
+      perView: 6,
+      spacing: 10,
+    }
+  })
 
   useEffect(() => {
       axios('http://localhost:3333/games')
@@ -22,7 +32,7 @@ function App() {
 
       <h1 className='text-6xl text-white font-black mt-20'>Seu <span className='text-transparent bg-duo-gradient bg-clip-text'>duo</span> está aqui.</h1>
 
-      <div className='grid grid-cols-6 gap-6 mt-16'>
+      <div className='keen-slider mt-16' ref={ref}>
         {games.map(game => {
           return (
             <GameBanner
